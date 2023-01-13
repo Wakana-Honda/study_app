255ee3d989c65f5f5b72966d2741d8b8c06fa5b5
EC2 Default User <wakana980830@gmail.com>
2023-01-13 11:10:43 +0000

    カレンダーも導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to:'tops#top'
+  resources:homes,only:[:index,:create,:show,:edit,:update,:destroy]
+  resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
